7a806e20f382029081dc40974d5bea973357ffe8
Ubuntu <ubuntu@ip-172-31-13-145.ap-south-1.compute.internal>
2025-08-08 09:27:30 +0000

    Add API root endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.http import JsonResponse
 from .views import test_view
 
+def root_view(request):
+    return JsonResponse({"message": "API root is working"})
+
 urlpatterns = [
+    path('', root_view),  # root endpoint
     path('test/', test_view),
 ]
-
