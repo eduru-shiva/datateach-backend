9712a18e919bdae64f562fb64a98c5d8c330c917
Ubuntu <ubuntu@ip-172-31-13-145.ap-south-1.compute.internal>
2025-08-12 10:08:06 +0000

    allow the temparray

--- backend_app/settings.py
+++ backend_app/settings.py
@@ -31,17 +31,21 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+         INSTALLED_APPS = [
+    'corsheaders',  # added for CORS
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',  # your app
+    'api',
 ]
 
 
 MIDDLEWARE = [
+    MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # added for CORS
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True  # only for testing
